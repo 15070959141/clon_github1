4d564bcd7cc781e4ade330cdca8f98cb0ff579b0
wucheng <3453446721@qq.com>
2022-08-01 16:03:06 +0800

    创建分支2

--- hellogit02/src/main/java/com/atguigu/helloGIT.java
+++ hellogit02/src/main/java/com/atguigu/helloGIT.java
@@ -2,7 +2,6 @@ package com.atguigu;
 
 public class helloGIT {
     public static void main(String[] args) {
-        System.out.println("更新1");
-        System.out.println("创建分支");
+        System.out.println("分支2");
     }
 }
