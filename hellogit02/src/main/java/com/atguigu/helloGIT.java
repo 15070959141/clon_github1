d95905ad0bd9835766746102116bfa92c426a6c4
wucheng <3453446721@qq.com>
2022-08-01 16:05:43 +0800

    分支3

--- hellogit02/src/main/java/com/atguigu/helloGIT.java
+++ hellogit02/src/main/java/com/atguigu/helloGIT.java
@@ -3,6 +3,6 @@ package com.atguigu;
 public class helloGIT {
     public static void main(String[] args) {
         System.out.println("分支2");
-        System.out.println("主线");
+        System.out.println("分支3");
     }
 }
